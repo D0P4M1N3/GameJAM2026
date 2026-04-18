4d2457fa8d6b8be6417b8d13d9465bb6a2f95fef
Jakkapat Nuchprayoon <116855767+D0P4M1N3@users.noreply.github.com>
2026-04-18 20:35:28 +0700

    Screen Flow template

---
+++ Assets/Script/Camera/ScreenFading.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenFading : MonoBehaviour
+{
+    public static ScreenFading Instance;
+
+    [Header("UI")]
+    [SerializeField] private Image blackScreen;
+
+    [Header("Fade Settings")]
+    public float fadeDuration = 1f;
+
+    [Header("Shader Settings")]
+    [SerializeField] private string valueProperty = "_Value";
+    [SerializeField] private float blackValue = 20f;
+    [SerializeField] private float clearValue = 0f;
+
+    private Material runtimeMaterial;
+    private Coroutine currentFade;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        if (blackScreen != null)
+        {
+            runtimeMaterial = Instantiate(blackScreen.material);
+            blackScreen.material = runtimeMaterial;
+            runtimeMaterial.SetFloat(valueProperty, blackValue);
+        }
+    }
+
+    private void Start()
+    {
+        StartCoroutine(FadeFromBlack());
+    }
+
+    public IEnumerator FadeToBlack()
+    {
+        yield return StartCoroutine(FadeRoutine(blackValue));
+    }
+
+    public IEnumerator FadeFromBlack()
+    {
+        yield return StartCoroutine(FadeRoutine(clearValue));
+    }
+
+    private IEnumerator FadeRoutine(float targetValue)
+    {
+        if (runtimeMaterial == null) yield break;
+
+        float startValue = runtimeMaterial.GetFloat(valueProperty);
+        float t = 0f;
+
+        while (t < fadeDuration)
+        {
+            t += Time.deltaTime;
+            float blend = t / fadeDuration;
+
+            float value = Mathf.Lerp(startValue, targetValue, blend);
+            runtimeMaterial.SetFloat(valueProperty, value);
+
+            yield return null;
+        }
+
+        runtimeMaterial.SetFloat(valueProperty, targetValue);
+        currentFade = null;
+    }
+}
