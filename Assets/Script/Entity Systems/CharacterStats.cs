2082359fbeb507c2168584e09047dbedaea4167f
Krittin Naliwong <banktandjosh@gmail.com>
2026-04-19 21:14:34 +0700

    Update CharacterStats.cs

--- Assets/Script/Entity Systems/CharacterStats.cs
+++ Assets/Script/Entity Systems/CharacterStats.cs
@@ -9,15 +9,18 @@ public class CharacterStats
     public float Speed = 5;
     public float MaxHP = 20;
     public float Damage = 1;
+    public float Storage = 10;
 
-    [Header("Multiplier")]
+    [Header("Modifier")]
     public float mSpeed = 1;
     public float mMaxHP = 1;
     public float mDamage = 1;
+    public float mStorage = 0;
 
 
     [Header("Finals")]
     public float finalSpeed => Speed * mSpeed;
     public float finalMaxHP => MaxHP * mMaxHP;
     public float finalDamage => Damage * mDamage;
+    public float finalStorage => Storage * mStorage;
 }
